f8a126a8051c04bbb00073de94ba408e37686145
Gustavo <163481672+Gustavoodilon@users.noreply.github.com>
2024-09-04 21:39:28 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,25 @@
+idade = 18
+
+if idade >= 18:
+    print("Maior de idade")
+
+print()
+
+idade1 = 16
+
+if idade1 >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+    
+print()    
+nota = 75
+
+if nota >= 90:
+    print("A")
+elif nota >= 80:
+    print("B")
+elif nota >= 70:
+    print("C")
+else:
+    print("D")
